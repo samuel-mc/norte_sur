0cf3a9b87ccd84faa27c4a8764d7abdd9d440c33
Samuel Medina <smdoomed@gmail.com>
2025-01-14 13:31:25 -0600

    Se agrega el hero

--- views/index.php
+++ views/index.php
@@ -1,7 +1,12 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/navbar.php'; ?>
 <main>
-    <section class="mw--1440">
+    <!-- Hero -->
+    <section class="mw--1440 hero">
+        <img src="<?php echo __ROOT__; ?>/public/img/home/hero.png" alt="hero" class="w-100">
+        <div class="hero__content align-content-center">
+            <h2 class="poppins-extralight text-uppercase text-white text--2xl text-center mw--720 mx-auto">La Revolución de la Cocina Mexicana</h2>
+        </div>
     </section>
 </main>
 <?php include 'partials/footer.php'; ?>
