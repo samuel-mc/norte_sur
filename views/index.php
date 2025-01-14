d2cd51e7b0d6ea5e94b7285a74c99be9c6884b8d
Samuel Medina <smdoomed@gmail.com>
2025-01-14 16:24:11 -0600

    Se agrego la seccion nuestro concepto

--- views/index.php
+++ views/index.php
@@ -10,7 +10,7 @@
     </section>
 
     <!-- Horario de servicio -->
-    <section class="mw--1440 row m-0">
+    <section class="mw--1440 row m-0 mx-auto">
         <div class="p-4 col-md-6 col-lg-4 align-content-center">
             <h3 class="poppins-bold text--lg mb-4 text-uppercase">Horarios de servicio</h3>
             <ul class="list-unstyled">
@@ -20,7 +20,7 @@
             </ul>
         </div>
         <div class="horarios--right col-md-6 col-lg-8 p-lg-5">
-            <div class="bg--black py-5 px-4 mw--600 mx-auto">
+            <div class="bg--black py-5 px-4 mw--600 mx-auto my-5">
                 <h3 class="poppins-semibold text--md mb-4 text-uppercase text-white text-center">Reserva tu mesa</h3>
                 <p class="poppins-regular text--sm text--white text-center">
                     Después de reservar llamaremos al cliente para confirmar, así que por favor ingrese su nombre y número de teléfono es obligatorio, ¡gracias!
@@ -36,15 +36,42 @@
                         <input type="number" class="horario--input" placeholder="Cantidad de personas">
                         <input type="date" class="horario--input" placeholder="Fecha*">
                     </div>
-                    <button class="btn btn--white poppins-regular rounded-0 mt-4">
+                    <button class="btn btn--white poppins-regular rounded-0 mt-4 w-100">
                         Reserva tu mesa
                     </button>
                 </div>
 
-                <button class="btn btn--white poppins-regular rounded-0" onclick="handleReservar()" id="btnReservar">
+                <button class="btn btn--white poppins-regular rounded-0 w-100" onclick="handleReservar()" id="btnReservar">
                     Reserva tu mesa
                 </button>
             </div>
+        </div>
+    </section>
+
+    <!-- Nuestro concepto -->
+    <section class="mw--1440 row m-0 mx-auto bg--ivory section__concepto">
+        <div class="col-3 p-0 concepto__first">
+            <img src="<?php echo __ROOT__; ?>/public/img/home/concepto1.png" alt="concepto" class="w-100">
+        </div>
+        <div class="col-6 py-5 px-4">
+            <div class="d-flex justify-content-center align-items-center">
+                <span class="line--red"></span>
+                <h3 class="text--md poppins-extralight text-uppercase mx-3">
+                    Nuestro concepto
+                </h3>
+                <span class="line--red"></span>
+            </div>
+            <div class="my-3">
+                <h2 class="text--lg poppins-regular text-center text-uppercase">Disfruta de un viaje de Norte a Sur de la República Mexicana</h2>
+            </div>
+            <div>
+                <p class="text--sm poppins-regular text-center">
+                NorteSur nace por la pasión y el amor a la gastronomía mexicana, por la gran variedad de tradiciones, colores, aromas y sabores que nos ofrece este hermoso país. Nuestra misión es traer a tu mesa los platillos más representativos de norte a sur de toda la república mexicana, reinventando siempre la cocina tradicional que tanto te gusta.
+                </p>
+            </div>
+        </div>
+        <div class="col-3 p-0 concepto__last">
+            <img src="<?php echo __ROOT__; ?>/public/img/home/concepto2.png" alt="concepto" class="w-100">
         </div>
     </section>
 </main>
