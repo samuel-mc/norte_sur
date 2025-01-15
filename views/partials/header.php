27cbc09c60674b5984c313ad21540777599b744e
Samuel Medina <smdoomed@gmail.com>
2025-01-14 18:42:10 -0600

    chore: Update header.php with font and favicon changes

--- views/partials/header.php
+++ views/partials/header.php
@@ -14,8 +14,10 @@ $title = isset($title) ? $title : 'Tanko';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- <link rel="stylesheet" href="<?php echo __ROOT__; ?>/public/js/splide/dist/css/splide.min.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- favicon png -->
+    <link rel="icon" href="<?php echo __ROOT__; ?>/public/img/favicon.png" type="image/x-icon" />
     <title><?php echo $title ?></title>
 </head>
 
